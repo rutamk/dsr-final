f60c9a61c9291da70cf84f90f58ca68bd4a048a0
rutamk <rutam2003@gmail.com>
2024-08-30 14:21:21 +0530

    vercel deplyment attempt 1 added vercel.json and changed urls

--- server/index.js
+++ server/index.js
@@ -19,7 +19,7 @@ app.use(express.json());
 
 app.use(
   cors({
-    origin: "http://localhost:5173",
+    origin: "https://dsr-final.vercel.app",
     methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
     allowedHeaders: ["Content-Type", "Authorization"],
     credentials: true,
