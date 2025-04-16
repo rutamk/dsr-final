66a2a78f1e215a9a140c5f08e9932aceff430c89
rutamk <rutam2003@gmail.com>
2025-04-17 00:15:18 +0530

    ADDED AWS S3 AND CLOUDFRONTTTT

--- server/index.js
+++ server/index.js
@@ -19,7 +19,7 @@ app.use(express.json());
 
 app.use(
   cors({
-    origin: "*",
+    origin: process.env.FRONTEND_URL,
     methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
     allowedHeaders: ["Content-Type", "Authorization"],
     // credentials: true,
